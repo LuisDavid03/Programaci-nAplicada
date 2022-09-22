09d7d44bb53a94f5585a68d7b86f5cf0960d07c6
LuisDavid03 <luisvillamarin@usantotomas.edu.co>
2022-09-22 11:41:52 -0500

    commit Usando Tkinter

--- Prueba.py
+++ Prueba.py
@@ -1,9 +1,5 @@
 
-edad = int(input("Ingrese un valor de EDAD:  "))
-#edad = 18
-if edad > 18:
-  print('Es mayor de edad')
-else:
-  print('Es menor de edad')
-
-print('Buenas TARDES')
+import tkinter as tk
+raiz = tk.Tk()
+raiz.title('KKLLEEIITTOONN')
+raiz.mainloop()
